8ec19c71611ae75df176082312b88f1617f41136
jcnoir <jcnoir@free.fr>
2012-07-14 21:25:16 +0200

    Initial commit

---
+++ src/test/java/TestJni.java
@@ -0,0 +1,20 @@
+import org.black.jtranscribe.generated.rubberband.RubberbandLibrary;
+import org.junit.Test;
+
+/**
+ * User: jcnoir
+ * Date: 14/07/12
+ */
+public class TestJni {
+
+    @Test
+    public void test_bridj_c() {
+
+        RubberbandLibrary.RubberBandState state;
+        state = RubberbandLibrary.rubberband_new(44100, 2, 0, 1.0, 1.0);
+        System.out.println("INIT done");
+        RubberbandLibrary.rubberband_reset(state);
+    }
+
+
+}
